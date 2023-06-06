9cecab97f9d2fb86cbfdeaa1ddc80170136939bc
fidel <fidelbello1997@gmail.com>
2023-06-06 18:28:13 -0400

    added character controller and character class

--- SandBox/Public/Characters/ASBCharacter.h
+++ SandBox/Public/Characters/ASBCharacter.h
@@ -4,26 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "MyCharacter.generated.h"
+#include "ASBCharacter.generated.h"
 
 UCLASS()
-class SANDBOX_API AMyCharacter : public ACharacter
+class SANDBOX_API ASBCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	AMyCharacter();
-
-protected:
-	// Called when the game starts or when spawned
+	ASBCharacter();
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
