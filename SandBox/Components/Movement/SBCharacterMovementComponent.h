6c810a61fc0ec3abeabeb6a7e2dd70080a08f901
fidel <fidelbello1997@gmail.com>
2023-06-15 19:21:44 -0400

    added movemoment component for future use

---
+++ SandBox/Components/Movement/SBCharacterMovementComponent.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "SBCharacterMovementComponent.generated.h"
+
+UCLASS()
+class SANDBOX_API USBCharacterMovementComponent: public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+public:
+	
+};
