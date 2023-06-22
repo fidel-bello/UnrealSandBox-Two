d334965f2da0b339995ee26c8c602124ce9f99c7
fidel <fidelbello1997@gmail.com>
2023-06-22 00:43:33 -0400

    added forward movement to controller

--- SandBox/SandBox.Build.cs
+++ SandBox/SandBox.Build.cs
@@ -8,7 +8,7 @@ public class SandBox : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent", "EnhancedInput"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
