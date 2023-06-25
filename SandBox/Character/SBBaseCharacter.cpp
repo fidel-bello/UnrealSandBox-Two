0dd76dc6f979dc5dd44a6872cfa14dbce6f18666
fidel <fidelbello1997@gmail.com>
2023-06-24 23:10:15 -0400

    refactored: made base character Forward action

--- SandBox/Character/SBBaseCharacter.cpp
+++ SandBox/Character/SBBaseCharacter.cpp
@@ -1,29 +1,24 @@
 ﻿#include "SBBaseCharacter.h"
-
-#include "InputActionValue.h"
 #include "Kismet/KismetMathLibrary.h"
-#include "SandBox/Components/Movement/SBCharacterMovementComponent.h"
+
 
 ASBBaseCharacter::ASBBaseCharacter(const FObjectInitializer& ObjectInitializer)
-	:Super(ObjectInitializer.SetDefaultSubobjectClass<USBCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
-{
-	SBCharacterMovementComponent = StaticCast<USBCharacterMovementComponent*>(GetCharacterMovement());
-}
+	: Super(ObjectInitializer){}
 
 void ASBBaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 }
 
-
-
 void ASBBaseCharacter::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 }
 
-void ASBBaseCharacter::ForwardMovementAction_Implementation(const FInputActionValue& Value)
+void ASBBaseCharacter::ForwardMovementAction_Implementation(float Value)
 {
 	const FRotator DirRotator(0.0f, AimingRotation.Yaw, 0.0f);
-	AddMovementInput(UKismetMathLibrary::GetForwardVector(DirRotator), Value.GetMagnitude());
+	AddMovementInput(UKismetMathLibrary::GetForwardVector(DirRotator), Value);
 }
+
+
--- SandBox/Character/SBBaseCharacter.h
+++ SandBox/Character/SBBaseCharacter.h
@@ -1,10 +1,7 @@
 ﻿#pragma once
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "SandBox/Components/Movement/SBCharacterMovementComponent.h"
 #include "SBBaseCharacter.generated.h"
-
-class USBCharacterMovementComponent;
 
 UCLASS(Abstract, NotBlueprintable)
 class SANDBOX_API ASBBaseCharacter: public ACharacter
@@ -12,21 +9,15 @@ class SANDBOX_API ASBBaseCharacter: public ACharacter
 
 	GENERATED_BODY()
 public:
-	ASBBaseCharacter(const FObjectInitializer& Initializer);
+	ASBBaseCharacter(const FObjectInitializer& ObjectInitializer);
+	
 	virtual void BeginPlay() override;
-	void Tick(float DeltaSeconds) override;
+	virtual void Tick(float DeltaSeconds) override;
 
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "SandBox|Input")
-	void ForwardMovementAction(const FInputActionValue& Value);
-	
-	FORCEINLINE USBCharacterMovementComponent* GetBaseCharacterMovementComponent() const { return SBCharacterMovementComponent; }
-	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="SandBox|Input")
+	void ForwardMovementAction(float Value);
+
 protected:
 
-	
-	// ReSharper disable once CppUE4ProbableMemoryIssuesWithUObject
-	USBCharacterMovementComponent* SBCharacterMovementComponent;
-
-	
 	FRotator AimingRotation = FRotator::ZeroRotator;
 };
